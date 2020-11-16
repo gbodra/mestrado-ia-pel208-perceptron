a7cbea2d996fdf378b5bee0c4b66a1d397092a82
Gustavo Bodra <gustavobodra@yahoo.com.br>
2020-11-16 11:30:06 -0300

    Exercicio finalizado

--- main.py
+++ main.py
@@ -1,51 +1,85 @@
-import sys
-import logging
-import pandas as pd
 import numpy as np
-
-logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
-
-
-# class Perceptron:
-#     def fit(self, x, y, n):
-#         cols = len(x)
-#         rows = len(y[4].unique())
-#         weights = np.zeros(rows, cols)
-#         x = x.to_numpy()
-#         y = y.to_numpy()
-#
-#         for i in range(cols):
-#             logging.debug("a")
-
-def fit(x, t):
-    x = x.to_numpy()
-    t = t.to_numpy()
-    w = np.zeros((4, 1))
-
-    y = perceptron_output(w, 1, x)
-    t_y = t - y
-    dw = n * np.dot(x.T, t_y)
-    w = w + dw
-
-    return w
+import pandas as pd
+import matplotlib.pyplot as plt
 
 
-def step_function(x):
-    return 1 if x >= 0 else 0
+def load_data():
+    """
+    Carrega os dados do dataset iris
+        :return: dados carregados em uma matriz
+    """
+    data = pd.read_csv("https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data", header=None)
+
+    # utiliza somente as duas primeiras classes
+    data = data[:100]
+    # transforma as classes em 0 e 1
+    data[4] = np.where(data.iloc[:, -1] == 'Iris-setosa', 0, 1)
+    data = np.asmatrix(data, dtype='float64')
+    return data
 
 
-def perceptron_output(weights, bias, x):
-    # calculation = np.dot(x, weights) + bias
-    calculation = np.dot(x, weights)
-    return calculation
-    # return step_function(calculation)
+def plot_data(data):
+    """
+    Exibe os dados
+        :param data: dados do dataset iris
+    """
+    plt.scatter(np.array(data[:50, 0]), np.array(data[:50, 2]), marker='o', label='setosa')
+    plt.scatter(np.array(data[50:, 0]), np.array(data[50:, 2]), marker='x', label='versicolor')
+    plt.xlabel('petal length')
+    plt.ylabel('sepal length')
+    plt.legend()
+    plt.show()
 
 
-iris_2_data = pd.read_csv("./data/iris_2_classes.txt", usecols=[0, 1, 2, 3], header=None)
-iris_2_label = pd.read_csv("./data/iris_2_classes.txt", usecols=[4], header=None)
-classes = {'setosa': 1, 'versicolor': -1}
-iris_2_label[4] = [classes[item] for item in iris_2_label[4]]
+def perceptron(data, num_iter):
+    """
+    Rede neural artificial: Perceptron
+        :param data: dados do dataset iris
+        :param num_iter: número de iterações
+    """
+    input = data[:, :-1]
+    labels = data[:, -1]
 
-n = 1
-result = fit(iris_2_data, iris_2_label)
-logging.debug(result)
+    # inicia o vetor de pesos com 0
+    w = np.zeros(shape=(1, input.shape[1] + 1))
+
+    erro_classificacao_epoch = []
+
+    for epoch in range(num_iter):
+        erro_classificacao = 0
+        for x, label in zip(input, labels):
+            x = np.insert(x, 0, 1)
+            y = np.dot(w, x.transpose())
+            if y > 0:
+                target = 1.0
+            else:
+                target = 0.0
+
+            delta = (label.item(0, 0) - target)
+
+            if delta:
+                erro_classificacao += 1
+                w += (delta * x)
+
+        erro_classificacao_epoch.append(erro_classificacao)
+
+    return w, erro_classificacao_epoch
+
+
+def plot_error(class_incorreto):
+    """
+    Exibe o erro ao longo das epochs
+        :param class_incorreto: dados classificados incorretamente
+    """
+    epochs = np.arange(1, num_iter + 1)
+    plt.plot(epochs, class_incorreto)
+    plt.xlabel('Iterações')
+    plt.ylabel('Classificados incorretamente')
+    plt.show()
+
+
+data = load_data()
+plot_data(data)
+num_iter = 10
+w, erro_classificacao = perceptron(data, num_iter)
+plot_error(erro_classificacao)
